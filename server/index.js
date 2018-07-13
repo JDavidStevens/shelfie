93dc54a6f589df9d54ce272b0a69b96f99b851a6
unknown <jdavid.stevens@gmail.com>
2018-07-13 14:55:18 -0600

    shelfiemidaftdy1

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
 // const conroller = require('controller');
 
 const app = express();
+massive(process.env.CONNECTION_STRING);
 
 const port = 4000;
 app.listen(port, () => {
--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      Image: '',
+      Name: '',
+      Price: ''
+    };
+  }
+
+  updateImage(val) {
+    this.setState({ Image: val });
+  }
+
+  updateName(val) {
+    this.setState({ Name: val });
+  }
+
+  updatePrice(val) {
+    this.setState({ Price: val });
+  }
+
   render() {
-    return <div>Form</div>;
+    return (
+      <div>
+        <input onChange={e => this.updateImage(e.target.value)} />
+        <input onChange={e => this.updateName(e.target.value)} />
+        <input onChange={e => this.updatePrice(e.target.value)} />
+        <button>Cancel</button>
+        <button>Add To Inventory</button>
+      </div>
+    );
   }
 }
