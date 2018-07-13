76a25ab800c572c7d0815b98c3112f69a2962166
unknown <jdavid.stevens@gmail.com>
2018-07-13 16:45:09 -0600

    shelfiefnlcmtdy1

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Product from '../Product/Product';
 
-export default class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        Dashboard
-        <Product />
-      </div>
-    );
-  }
+export default function Dashboard(props) {
+  // render() {
+  return (
+    <div>
+      Dashboard
+      <Product />
+    </div>
+  );
+  // }
 }
